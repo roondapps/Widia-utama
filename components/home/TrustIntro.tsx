a96ed8dc0f99016d9497d133b570e1e4b54dc16a
roondapps <roondapps@gmail.com>
2026-09-16 00:07:47 +0700

    update

--- components/home/TrustIntro.tsx
+++ components/home/TrustIntro.tsx
@@ -48,6 +48,11 @@ export function TrustIntro() {
     alt="Klien 2"
     className="h-12 w-auto object-contain"
   />
+            <img
+              src="/images/clients/client3.png"
+              alt="client3"
+              classname="h-12 w-auto bbject-contain"
+              />
 </div>
         </Reveal>
       </Container>
